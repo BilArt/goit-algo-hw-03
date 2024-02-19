77a2ade61b39ff5cd7f7def7c2b848e5dc8356b4
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-02-19 17:33:07 +0100

    task2: added main()

--- task2.py
+++ task2.py
@@ -8,3 +8,26 @@ def koch_snowflake(t, order, size):
             koch_snowflake(t, order-1, size/3)
             t.left(angle)
 
+def main():
+    screen = turtle.Screen()
+    screen.setup(width=800, height=800)
+    screen.bgcolor("white")
+    screen.title("Koch Snowflake")
+
+    t = turtle.Turtle()
+    t.speed(0)
+    t.penup()
+    t.goto(-150, 90)
+    t.pendown()
+    t.color("blue")
+
+    order = int(input("Enter the order of the Koch snowflake: "))
+    size = 300
+    for _ in range(3):
+        koch_snowflake(t, order, size)
+        t.right(120)
+
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
